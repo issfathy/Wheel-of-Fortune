b402fee3b9f1e8c540736fc0389b3d0a19730b40
issfathy <91920608+issfathy@users.noreply.github.com>
2021-10-12 17:44:32 -0400

    The basics for the project
    Shreya has completed the basics of the wheel of fortune. 
    We will start building more on this code

---
+++ WheelOfFortune.py
@@ -0,0 +1,39 @@
+import random 
+
+word = ["halloween", "christmas", "easter"]
+clue = ["Holiday on October 30th", "Santa Claus", "Rabbits!"]
+letterIDK = ""
+ENDLETTERS = ""
+
+chosenWord = random.choice(word)
+chosenClue = clue[word.index(chosenWord)]
+
+print(chosenClue)
+
+guessingWord = []
+for i in chosenWord:
+    guessingWord.append("_ ")
+
+for i in guessingWord:
+    letterIDK += i
+print(letterIDK)
+
+lettersGuessed = ""
+numberOfGuesses = 0
+while(numberOfGuesses <= 10): #stop loop if guessingWord = word
+    if(ENDLETTERS == chosenWord):
+        print("You win!")
+        break
+    numberOfGuesses += 1
+    letter = input("Guess a letter: ")
+    lettersGuessed += letter
+    print(lettersGuessed)
+
+    ENDLETTERS = ""
+    for i in range(len(chosenWord)):
+        if chosenWord[i] == letter:
+            guessingWord[i] = letter
+    for i in guessingWord:
+        ENDLETTERS += i
+    print(ENDLETTERS)
+
