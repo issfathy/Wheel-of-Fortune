69924ec668d83757e16ffddee43f7dbc0cd9bc48
issfathy <91920608+issfathy@users.noreply.github.com>
2021-10-12 22:00:56 -0400

    Started to work on multiplayer functions

---
+++ WheelOfFortune.py
@@ -0,0 +1,79 @@
+import random
+
+
+class colors:
+    correctGreen = '\033[92m'
+    wrongRed = '\033[91m'
+    whiteBasic = '\033[97m'
+
+
+class player:
+    def __init__(self, name, prizeMoney=0) -> None:
+        self.name = name
+        self.prizeMoney = prizeMoney
+
+    def addMoney(self, bank):
+        self.prizeMoney += bank
+
+
+def getCorrectInput(uinput, min, max):
+    userinput = input(uinput)
+
+    while True:
+        try:
+            x = int(userinput)
+            if x < min:
+                errmessage = 'Must be at least {}'.format(min)
+            elif x > max:
+                errmessage = 'Must be at most {}'.format(max)
+            else:
+                return x
+        except ValueError:
+            errmessage = '{} is not a number.'.format(userinput)
+
+        userinput = input('{}\n{}'.format(errmessage, uinput))
+
+
+NumPlayers = getCorrectInput("How many players are playing?", 1, 10)
+
+
+word = ["halloween", "christmas", "easter"]
+clue = ["Holiday on October 30th", "Santa Claus", "Rabbits!"]
+letterIDK = ""
+ENDLETTERS = ""
+
+chosenWord = random.choice(word)
+chosenClue = clue[word.index(chosenWord)]
+
+print(chosenClue)
+
+guessingWord = []
+for i in chosenWord:
+    guessingWord.append("_ ")
+
+for i in guessingWord:
+    letterIDK += i
+print(letterIDK)
+
+lettersGuessed = ""
+numberOfGuesses = 0
+while(numberOfGuesses <= 10):  # stop loop if guessingWord = word
+    if(ENDLETTERS == chosenWord):
+        print(colors.correctGreen + "You win!" + colors.whiteBasic)
+        break
+    numberOfGuesses += 1
+    guess = input("Guess a letter: ")
+    letter = guess.lower()
+    lettersGuessed += letter
+    print(lettersGuessed)
+
+    ENDLETTERS = ""
+    for i in range(len(chosenWord)):
+        if chosenWord[i] == letter:
+            guessingWord[i] = letter
+
+    for i in guessingWord:
+        ENDLETTERS += i
+    print(ENDLETTERS)
+
+print(colors.whiteBasic)
