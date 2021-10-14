aacd55125d0b762a08d689f28af1d8e37cbb8ddb
notshrez <76530253+notshrez@users.noreply.github.com>
2021-10-14 11:37:08 -0400

    added comment for changes

--- WheelOfFortune.py
+++ WheelOfFortune.py
@@ -1,47 +1,4 @@
-import random
-
-
-class colors:
-    correctGreen = '\033[92m'
-    wrongRed = '\033[91m'
-    whiteBasic = '\033[97m'
-
-
-class player:
-    def __init__(self, name, bank):
-        self.name = name
-        self.bank = bank
-        self.prizes = []
-
-    def addMoney(self, bank):
-        self.prizeMoney += bank
-
-    def goBankrupt(self):
-        self.prizeMoney = 0
-
-    def addPrize(self, prize):
-        self.prizes.append(prize)
-
-
-def getCorrectInput(uinput, min, max):
-    userinput = input(uinput)
-
-    while True:
-        try:
-            x = int(userinput)
-            if x < min:
-                errmessage = 'Must be at least {}'.format(min)
-            elif x > max:
-                errmessage = 'Must be at most {}'.format(max)
-            else:
-                return x
-        except ValueError:
-            errmessage = '{} is not a number.'.format(userinput)
-
-        userinput = input('{}\n{}'.format(errmessage, uinput))
-
-
-NumPlayers = getCorrectInput("How many players are playing?", 1, 10)
+import random 
 
 
 class colors:
@@ -104,6 +61,7 @@ while(numberOfGuesses <= 10):  # stop loop if guessingWord = word
         print(colors.correctGreen + "You win!" + colors.whiteBasic)
         break
     numberOfGuesses += 1
+    #can't guess a letter twice
     guess = input("Guess a letter: ")
     letter = guess.lower()
     lettersGuessed += letter
